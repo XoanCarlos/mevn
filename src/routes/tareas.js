19187c4baa7605b36d8007c9686eb0ec216b311a
XoanCarlos <jcarlosperezgonzalez@gmail.com>
2023-01-28 19:32:07 +0100

    Configuración Servidor-4

--- src/routes/tareas.js
+++ src/routes/tareas.js
@@ -8,5 +8,27 @@ router.get('/', async (req, res) => {
     res.json(tareas);
 });
 
+router.post('/', async (req, res) =>{
+   const tarea = new Tarea(req.body);
+   await tarea.save();
+   res.json({
+       status: 'Tarea creada'
+   });
+});
 
+
+router.put('/:id', async (req, res) => {
+    await  Tarea.findByIdAndUpdate(req.params.id, req.body);
+    res.json({
+        status: 'Tarea modificada'
+    });
+});
+
+router.delete('/:id', async (req, res) => {
+    await  Tarea.findByIdAndRemove(req.params.id);
+
+    res.json({
+        status: 'Tarea Eliminada'
+    });
+});
 module.exports = router;
